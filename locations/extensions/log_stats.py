d0ffe083fed889fe98c8ff3d0d4271c36b59312f
ebadm <ebadm.work@gmail.com>
2024-07-05 17:05:18 +0300

    fix: deploy ATP on Zyte with GCP compatibility

--- locations/extensions/log_stats.py
+++ locations/extensions/log_stats.py
@@ -31,4 +31,6 @@ class LogStatsExtension:
 
         if filename:
             with open(filename, "w") as f:
-                f.write(json.dumps(self.crawler.stats.get_stats(), default=myconverter, sort_keys=True, indent=1))
+                f.write(
+                    json.dumps(self.crawler.stats.get_stats(), default=myconverter, sort_keys=True, indent=1)
+                )
